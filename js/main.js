c222b697abd214d8ebe439c936f58d3122dc85a7
dshergill <diljotshergill@hotmail.com>
2017-04-09 14:49:58 -0600

    Account details updated, js function to hide sign-in

--- js/main.js
+++ js/main.js
@@ -124,15 +124,7 @@ $(function() {
 
 });
 
-
-$('#showDialog').click(function() {
-    $('#dialog').dialog(
-        {
-            open: function() {
-                $(this).load('account-details.html');
-            },
-            modal: true
-        }
-    );
-    $('#dialog').dialog('open');
+$(document).ready(function(e) {
+    $("#login-link").hide();
 });
+
