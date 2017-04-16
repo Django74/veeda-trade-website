df6f305290701c4a67ffe881a26e5b264bd8173b
archan <archan@ucalgary.ca>
2017-04-15 20:02:57 -0600

    basic admin stuff

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,17 +1,35 @@
-// Add Firebase Admin SDK
-var admin = require("firebase-admin");
+var http = require('http');
 
-// Initialize the SDK
-var serviceAccount = require("veeda-8bc58-firebase-adminsdk-1qjy6-70120435dd.json");
+var server = http.createServer(function(req, res) {
+  // Add Firebase Admin SDK
+  var admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://veeda-8bc58.firebaseio.com"
+  // Initialize the SDK
+  var serviceAccount = require("veeda-8bc58-firebase-adminsdk-1qjy6-70120435dd.json");
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://veeda-8bc58.firebaseio.com"
+  });
+
+  // uid for John Doe
+  var uid = "etmu7YOuG1W5D3hNHnqdxIKm0853";
+  admin.auth().getUser(uid)
+    .then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log("Successfully fetched user data:", userRecord.toJSON());
+      res.writeHead(200);
+      res.write("Hello World\n");
+      res.end(userRecord.displayName);
+    })
+    .catch(function(error) {
+      console.log("Error fetching user data:", error);
+    });
 });
 
-// Delete a user
-$('#deleteUser').click(function(e) {
-  // TODO: Retrieve uid
+server.listen(3000);
+
+/*
   var uid = document.getElementById('some-user-id').value;
 
   admin.auth().deleteUser(uid)
@@ -22,3 +40,4 @@ $('#deleteUser').click(function(e) {
       console.log("Error deleting user: ", error);
     });
 })
+*/
